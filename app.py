6e4afa9e4134c1f28366f43f37226d7da40686a2
Chaitanya <mohitchaitanya@gmail.com>
2021-05-05 15:23:45 +0530

    Final

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request
+from flask_cors import CORS
+import numpy as np
+import pandas as pd
+import torch
+from torch.autograd import Variable
+import pickle
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"//*": {"origins": "*"}})
+
+authKey = "123authKey123"
+
+
+@app.route('/')
+def home():
+  return "I like food better than your face"
+
+@app.route('/predict')
+def predict():
+  key = request.headers.get('authKey')
+  if key != authKey:
+    return {
+      "msg":"Invalid Access!!!"
+    }
+  res = request.get_json()
+  input_arr = res['input_arr']
+  input_arr = torch.FloatTensor(input_arr)
+  user_input = Variable(input_arr).unsqueeze(0)
+  model = pickle.load(open('models/RBM_Model.sav','rb'))
+  output = model.predict(user_input)
+  output = output.data.numpy().tolist()
+  return {
+    "Output":output
+  }
+
+if __name__ == '__main__':
+  app.run(debug = False)
