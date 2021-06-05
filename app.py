8adee6cb3c31afc13e81ba8e27a26b5712dc65a1
Chaitanya <mohitchaitanya@gmail.com>
2021-06-05 20:16:38 +0530

    Added Files

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 import pickle
+from rbm  import RBM
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -11,33 +12,6 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 authKey = "123authKey123"
 
 
-class RBM():
-  def __init__(self, nv, nh):
-    self.W = torch.randn(nh, nv)
-    self.a = torch.randn(1, nh)
-    self.b = torch.randn(1, nv)
-
-  def sample_h(self, x):
-    wx = torch.mm(x, self.W.t())
-    activation = wx + self.a.expand_as(wx)
-    p_h_given_v = torch.sigmoid(activation)
-    return p_h_given_v, torch.bernoulli(p_h_given_v)
-
-  def sample_v(self, y):
-    wy = torch.mm(y, self.W)
-    activation = wy + self.b.expand_as(wy)
-    p_v_given_h = torch.sigmoid(activation)
-    return p_v_given_h, torch.bernoulli(p_v_given_h)
-
-  def train(self, v0, vk, ph0, phk):
-    self.W += (torch.mm(v0.t(),ph0) - torch.mm(vk.t(),phk)).t()
-    self.b += torch.sum((v0 - vk), 0)
-    self.a += torch.sum((ph0 - phk), 0)
-    
-  def predict(self, x):
-    _, h = self.sample_h(x)
-    _, v = self.sample_v(h)
-    return v
 
 @app.route('/')
 def home():
